de2474ce90b95b2232e274fbecd99ec2640d8fa1
Julio Villanueva <wolfri2002@gmail.com>
2022-06-07 22:17:53 -0500

    Correción de código

--- temp.php
+++ temp.php
@@ -1,5 +1,5 @@
 <?php
 
-	move_uploaded_file($_FILES['archivo']['tmp_name'],"registro-productos/img/".$_FILES['archivo']['name']);
+	//move_uploaded_file($_FILES['archivo']['tmp_name'],"registro-productos/img/".$_FILES['archivo']['name']);
 
 ?>
